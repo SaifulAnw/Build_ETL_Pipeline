bbd8ebd57a2ffa327ad17932dc8131ba3d21a0e6
Saiful Anwar <117976868+saifulanwar30@users.noreply.github.com>
2023-12-08 03:56:39 +0700

    Add files via upload

---
+++ DimProductSubCategory.sql
@@ -0,0 +1,4 @@
+SELECT TOP (1000) [EnglishProductSubcategoryName]
+      ,[SpanishProductSubcategoryName]
+      ,[FrenchProductSubcategoryName]
+  FROM [AdventureWorksDW2019].[dbo].[DimProductSubcategory]
---
+++ PersonContactType.sql
@@ -0,0 +1,3 @@
+SELECT TOP (1000) [Name]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2022].[Person].[ContactType]
